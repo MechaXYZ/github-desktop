b8e7257b0e35d4b5ec217600ad91417a2536126e
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2023-12-01 11:54:38 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ NEEDED="tigervnc-tools"
 if [ $(dpkg-query -W -f='${Status}' $NEEDED 2>/dev/null | grep -c "ok installed") == 0 ]; then
   echo "Unable to find package '$NEEDED.' Setting up $NEEDED."
   sudo apt-get --yes install $NEEDED
+
+  echo "Installing firefox-esr and libpci-dev as firefox needs the snap version and google-chrome is buggy."
+  sudo add-apt-repository --yes ppa:mozillateam/ppa
+  sudo apt-get --yes install firefox-esr libpci-dev
 fi
 
 DIR="/opt/novnc/utils/novnc_proxy"
@@ -15,5 +19,4 @@ if [ ! -f $DIR ]; then
 fi
 
 tigervncserver :0 -SecurityTypes None -rfbport 5901
-/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen localhost:6080 &
-echo "Go to port 6080 to view your desktop."
+/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen localhost:6080 && echo "Go to port 6080 to view your desktop." &
